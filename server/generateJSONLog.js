9589d86973ad6da3c55bf5fc1985551583069792
Kevin Herklotz <scheek.hee@gmail.com>
2018-08-09 13:53:16 +0200

    added comments

--- server/generateJSONLog.js
+++ server/generateJSONLog.js
@@ -4,34 +4,40 @@ const readline = require('readline');
 var mkdirp = require('mkdirp');
 const parseLineToJSON = require('./parseLineToJSON');
 
-const generateJSONLog = (sourceLocation, destinationLocation) => {
-
-  const destinationPath = destinationLocation.substring(0, destinationLocation.lastIndexOf('/'));
-  mkdirp(destinationPath, function (err) {
+const generateJSONLog = (sourcePath, destinationPath) => {
+  // remove file name to get destination path
+  const destinationFolder = destinationPath.substring(0, destinationPath.lastIndexOf('/'));
+  // Create destination folder if it doesn't exist
+  mkdirp(destinationFolder, function (err) {
     if (err) {
-      console.error(`Error: Couldn't create folder "${destinationPath}"`, err)
+      console.error(`Error: Couldn't create folder "${destinationFolder}"`, err)
     }
   });
 
+  // create ReadStream
   const lineReader = readline.createInterface({
-    input: fs.createReadStream(sourceLocation, 'UTF-8')
+    input: fs.createReadStream(sourcePath, 'UTF-8')
   });
 
+  // Array that will contain all the JSON objects
   const LogJSONArray = [];
 
+  // for each read line...
   lineReader.on('line', function (line) {
+    // ... generate JSON and add to array
     LogJSONArray.push(parseLineToJSON(line));
   });
 
+  // when finished with reading the file...
   lineReader.on('close', function () {
     const content = JSON.stringify(LogJSONArray);
-
-    fs.writeFile(destinationLocation, content, 'utf8', (err) => {
+    // ... write array into file
+    fs.writeFile(destinationPath, content, 'utf8', (err) => {
         if (err) {
           return console.log(err);
         }
 
-        console.log(`The JSON array was successfully geneated and saved in ${destinationLocation}`);
+        console.log(`The JSON array was successfully geneated and saved in ${destinationPath}`);
     });
   });
 
