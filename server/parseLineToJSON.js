3acebd5a16dd32c5bbadb934719562434a05af4d
Kevin Herklotz <scheek.hee@gmail.com>
2018-08-09 12:52:48 +0200

    parseLineToJSON()

--- server/parseLineToJSON.js
+++ server/parseLineToJSON.js
@@ -1,7 +1,37 @@
 'use strict';
 
-const parseLineToJSON = line => {
-  return 'TEST'
+
+const parseLineToJSON = logLine => {
+  // remove quotation marks and square brackets
+  logLine = logLine.replace(/\"|\[|\]/g, '');
+
+  // replace colons with space (to split it up easier later)
+  logLine = logLine.replace(/\:/g, ' ');
+
+  // split string up into array
+  const logArray = logLine.split(' ');
+
+  // generate object
+  const JSONObject = {
+    'host': logArray[0],
+    'datetime': {
+      'day': logArray[1],
+      'hour': logArray[2],
+      'minute': logArray[3],
+      'second': logArray[4]
+    },
+    'request': {
+      'method': logArray[5],
+      'url': logArray[6],
+      'protocol': logArray[7].split('/')[0],
+      'protocol_version': logArray[7].split('/')[1]
+    },
+    'response_code': logArray[8],
+    'document_size': logArray[9]
+  };
+
+  return JSON.stringify(JSONObject);
+
 };
 
 module.exports = parseLineToJSON;
