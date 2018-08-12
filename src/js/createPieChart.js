3a5356a350e5a5ea1acc66a782d8e3e2ea95a504
Kevin Herklotz <scheek.hee@gmail.com>
2018-08-12 21:22:44 +0200

    added number to label

--- src/js/createPieChart.js
+++ src/js/createPieChart.js
@@ -9,9 +9,11 @@
 const createPieChart = (log, valueToAnalyze, ElementID, colors) => {
   const logValues = new Map();
 
+  // for each JSON object
   log.forEach((dataset) => {
 
     let logValue = dataset;
+    // get the actual value that should be analyzed and store it into logValue
     valueToAnalyze.forEach((analyzeValue) => {
       logValue = logValue[analyzeValue];
     });
@@ -21,15 +23,24 @@ const createPieChart = (log, valueToAnalyze, ElementID, colors) => {
     }
 
     if (logValues.has(logValue)) {
+      // increment count
       let currentVal = logValues.get(logValue);
       logValues.set(logValue, currentVal + 1);
     } else {
+      // set new value with count of 1
       logValues.set(logValue, 1);
     }
   });
 
-  const logValueArray = Array.from(logValues.keys())
-  const countArray = Array.from(logValues.values())
+  // const logValueArray = Array.from(logValues);
+  // const countArray = Array.from(logValues.values());
+
+  const logValueArray = [];
+  const countArray = [];
+  logValues.forEach((value, key) => {
+    logValueArray.push(`${key}: ${value}`);
+    countArray.push(value);
+  });
 
   // create actual pie chart
   new Chart(document.getElementById(ElementID), {
