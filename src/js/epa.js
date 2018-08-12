4a937aa12495a6c94ec8ad474c1aeae675cb8ed7
Kevin Herklotz <scheek.hee@gmail.com>
2018-08-12 18:08:13 +0200

    added basic pie chart

--- src/js/epa.js
+++ src/js/epa.js
@@ -1,18 +1,53 @@
 import log from '../dist/log.js';
 
-console.log(log)
+
+const methodDistribution = (log) => {
+  const methods = new Map();
+
+  log.forEach((dataset) => {
+    const method = dataset.request.method;
+
+    method === "cons/circle_logo_small.gif" ? console.log(dataset) : null;
+
+    if (methods.has(method)) {
+      let currentVal = methods.get(method);
+      methods.set(method, currentVal + 1);
+    } else {
+      methods.set(method, 1);
+    }
+  });
+
+  console.log(methods);
+  const methodArray = Array.from(methods.keys())
+  const countArray = Array.from(methods.values())
+
+  // pie chart
+  const ctx = document.getElementById("methodChart");
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: methodArray,
+        datasets: [{
+            data: countArray,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ]
+        }]
+    }
+  });
+
+}
 
 const requestsPerMinute = (log) => {
   // bar chart
-
 }
 
-const methodDistribution = (log) => {
-  // pie chart
-
-}
-
-const answerCodeDistribution = (log) => {
+const responseCodeDistribution = (log) => {
   // pie chart
 
 }
@@ -21,3 +56,5 @@ const sizeDistribution = (log) => {
   // ??? chart
 
 }
+
+methodDistribution(log);
